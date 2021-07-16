feaec0339596837dd57eb25205bec5919b37a954
emilioCode <42071530+emilioCode@users.noreply.github.com>
2021-07-16 13:14:30 -0400

    Dice Roller Button functionality available

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.diceroll
 
 import android.os.Bundle
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+//            toast.show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
